68d5ebbe510b99c06a95d594bd195e045e6e0576
Kim Hart <kimhart90@gmail.com>
2016-01-23 17:43:58 -0500

    instagram and photos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = 'mongodb://localhost:27017/parkdash';
 var ObjectId = require('mongodb').ObjectId;
+var ig = require('instagram-node').instagram();
 
 var db;
  
@@ -11,6 +12,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+ig.use({ access_token: 'YOUR_ACCESS_TOKEN' });
+ig.use({ client_id: 'YOUR_CLIENT_ID',
+         client_secret: 'YOUR_CLIENT_SECRET' });
+
+
 
 MongoClient.connect(mongoUrl, function(err, database){
   if(err){
---
+++ public/js/park.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+  console.log('park.js linked!')
+
+  $('.slideshow').cycle({
+    fx: 'fade',
+    pause:   1,
+    speed: 4000,
+    timeout:  3500
+  });
+
+
+
+});
